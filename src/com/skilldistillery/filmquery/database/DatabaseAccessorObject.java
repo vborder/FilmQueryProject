48a7c1b98f9dfb2031a58b20bd3d1663f59ad5c2
Vincent Borden <vinbor81@gmail.com>
2020-05-01 20:14:53 -0400

    initial commit

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,79 @@
+package com.skilldistillery.filmquery.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.skilldistillery.filmquery.entities.Actor;
+import com.skilldistillery.filmquery.entities.Film;
+
+public class DatabaseAccessorObject implements DatabaseAccessor {
+	
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+  @Override
+  public Film findFilmById(int filmId) {
+    return null;
+  }
+  
+  public Actor findActorById(int actorId) {
+  Actor actor = null;
+  //String user/pass/Connection conn = DriverManager.getConnection
+  String sql = "SELECT id, first_name, last_name FROM actor WHERE id = ?";
+  PreparedStatement stmt = conn.prepareStatement(sql);
+  stmt.setInt(1,actorId);
+  ResultSet actorResult = stmt.executeQuery();
+  if (actorResult.next()) {
+    actor = new Actor(); // Create the object
+    // Here is our mapping of query columns to our object fields:
+    actor.setId(actorResult.getInt(1));
+    actor.setFirstName(actorResult.getString(2));
+    actor.setLastName(actorResult.getString(3));
+  }
+  //...
+  return actor;
+  
+  public List<Film> findFilmsByActorId(int actorId) {
+	  List<Film> films = new ArrayList<>();
+	  try {
+	    Connection conn = DriverManager.getConnection(url, user, pass);
+	    String sql = "SELECT id, title, description, release_year, language_id, rental_duration, ";
+	                sql += " rental_rate, length, replacement_cost, rating, special_features "
+	               +  " FROM film JOIN film_actor ON film.id = film_actor.film_id "
+	               + " WHERE actor_id = ?";
+	    PreparedStatement stmt = conn.prepareStatement(sql);
+	    stmt.setInt(1, actorId);
+	    ResultSet rs = stmt.executeQuery();
+	    while (rs.next()) {
+	      int filmId = rs.getInt(1);
+	      String title = rs.getString(2);
+	      String desc = rs.getString(3);
+	      short releaseYear = rs.getShort(4);
+	      int langId = rs.getInt(5);
+	      int rentDur = rs.getInt(6);
+	      double rate = rs.getDouble(7);
+	      int length = rs.getInt(8);
+	      double repCost = rs.getDouble(9);
+	      String rating = rs.getString(10);
+	      String features = rs.getString(11);
+	      Film film = new Film(filmId, title, desc, releaseYear, langId,
+	                           rentDur, rate, length, repCost, rating, features);
+	      films.add(film);
+	    }
+	    rs.close();
+	    stmt.close();
+	    conn.close();
+	  } catch (SQLException e) {
+	    e.printStackTrace();
+	  }
+	  return films;
+	}
+
+}
---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,166 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	private int id;
+	private String title;
+	private String description;
+	private Integer releaseYear;
+	private int languageId;
+	private int rentalDuration;
+	private double rentalRate;
+	private Integer length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+	public int getId() {
+		return id;
+	}
+	
+	public Film() {
+		
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getReleaseYear() {
+		return releaseYear;
+	}
+	public void setReleaseYear(Integer releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+	public int getLanguageId() {
+		return languageId;
+	}
+	public void setLanguageId(int languageId) {
+		this.languageId = languageId;
+	}
+	public int getRentalDuration() {
+		return rentalDuration;
+	}
+	public void setRentalDuration(int rentalDuration) {
+		this.rentalDuration = rentalDuration;
+	}
+	public double getRentalRate() {
+		return rentalRate;
+	}
+	public void setRentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+	public Integer getLength() {
+		return length;
+	}
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+	public double getReplacementCost() {
+		return replacementCost;
+	}
+	public void setReplacementCost(double replacementCost) {
+		this.replacementCost = replacementCost;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+	public void setSpecialFeatures(String specialFeatures) {
+		this.specialFeatures = specialFeatures;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + languageId;
+		result = prime * result + ((length == null) ? 0 : length.hashCode());
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
+		result = prime * result + ((releaseYear == null) ? 0 : releaseYear.hashCode());
+		result = prime * result + rentalDuration;
+		long temp;
+		temp = Double.doubleToLongBits(rentalRate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(replacementCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((specialFeatures == null) ? 0 : specialFeatures.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (languageId != other.languageId)
+			return false;
+		if (length == null) {
+			if (other.length != null)
+				return false;
+		} else if (!length.equals(other.length))
+			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
+			return false;
+		if (releaseYear == null) {
+			if (other.releaseYear != null)
+				return false;
+		} else if (!releaseYear.equals(other.releaseYear))
+			return false;
+		if (rentalDuration != other.rentalDuration)
+			return false;
+		if (Double.doubleToLongBits(rentalRate) != Double.doubleToLongBits(other.rentalRate))
+			return false;
+		if (Double.doubleToLongBits(replacementCost) != Double.doubleToLongBits(other.replacementCost))
+			return false;
+		if (specialFeatures == null) {
+			if (other.specialFeatures != null)
+				return false;
+		} else if (!specialFeatures.equals(other.specialFeatures))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Film [id=" + id + ", title=" + title + ", description=" + description + ", releaseYear=" + releaseYear
+				+ ", languageId=" + languageId + ", rentalDuration=" + rentalDuration + ", rentalRate=" + rentalRate
+				+ ", length=" + length + ", replacementCost=" + replacementCost + ", rating=" + rating
+				+ ", specialFeatures=" + specialFeatures + "]";
+	}
+	
+	
+	
+}
